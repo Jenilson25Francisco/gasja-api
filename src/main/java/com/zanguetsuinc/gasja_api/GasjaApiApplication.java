68b0519d8e2ed62df0595d46f261b26a461b42f0
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-07 18:37:44 +0100

    initialize gasja-api project with Spring Boot and PostgreSQL setup

---
+++ src/main/java/com/zanguetsuinc/gasja_api/GasjaApiApplication.java
@@ -0,0 +1,13 @@
+package com.zanguetsuinc.gasja_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasjaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasjaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zanguetsuinc/gasja_api/GasjaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanguetsuinc.gasja_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasjaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
