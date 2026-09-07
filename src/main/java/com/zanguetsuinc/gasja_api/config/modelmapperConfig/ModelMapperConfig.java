db7a909ed8cbeff1404f0da325fff0a1de9f7b7f
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-07 19:14:25 +0100

    add ModelMapper configuration for object mapping

---
+++ src/main/java/com/zanguetsuinc/gasja_api/config/modelmapperConfig/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.zanguetsuinc.gasja_api.config.modelmapperConfig;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
