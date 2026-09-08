b41a16c913cd50e33a41f96379dab4d09623270d
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-08 14:06:53 +0100

    add CreateUserService for user creation and retrieval

---
+++ src/main/java/com/zanguetsuinc/gasja_api/domain/services/CreateUserService.java
@@ -0,0 +1,36 @@
+package com.zanguetsuinc.gasja_api.domain.services;
+
+import com.zanguetsuinc.gasja_api.domain.exceptions.BusinessException;
+import com.zanguetsuinc.gasja_api.domain.exceptions.ResourceNotFoundException;
+import com.zanguetsuinc.gasja_api.domain.models.User;
+import com.zanguetsuinc.gasja_api.domain.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+public class CreateUserService {
+
+    private final UserRepository userRepository;
+
+    public CreateUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUser(UUID userId){
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Utilizador nao encontrado"));
+    }
+
+    @Transactional
+    public User createUser(User user){
+
+        boolean existingUser = userRepository.findByEmail(user.getEmail()).isPresent();
+        if(existingUser){
+            throw new BusinessException("Já existe um utilizador com o email " + user.getEmail());
+        }
+
+        return userRepository.save(user);
+    }
+}
