505a9cf260d64e2f9e315f1d1b6b25bcc629fd6c
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-08 14:06:07 +0100

    add User entity and UserRepository for user management

---
+++ src/main/java/com/zanguetsuinc/gasja_api/domain/models/User.java
@@ -0,0 +1,36 @@
+package com.zanguetsuinc.gasja_api.domain.models;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Users")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "created_at", updatable = false)
+    private OffsetDateTime updateAt;
+
+}
---
+++ src/main/java/com/zanguetsuinc/gasja_api/domain/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.zanguetsuinc.gasja_api.domain.repositories;
+
+import com.zanguetsuinc.gasja_api.domain.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+}
