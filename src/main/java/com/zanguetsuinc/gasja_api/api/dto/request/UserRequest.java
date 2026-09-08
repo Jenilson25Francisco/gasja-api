eea4da054c891e7174583643cbb5fcbc68f83c8e
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-08 15:42:20 +0100

    feat: add validations on userResponse object

--- src/main/java/com/zanguetsuinc/gasja_api/api/dto/request/UserRequest.java
+++ src/main/java/com/zanguetsuinc/gasja_api/api/dto/request/UserRequest.java
@@ -1,5 +1,8 @@
 package com.zanguetsuinc.gasja_api.api.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +10,14 @@ import lombok.Setter;
 @Setter
 public class UserRequest {
 
+    @NotBlank(message = "O nome completo é obrigatório")
+    @Size(min = 3, message = "O campo nome deve ter no mínimo {min} caracteres")
     private String name;
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Digite um e-mail válido")
     private String email;
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 5, message = "A senha deve ter no mínimo {min} caracteres")
     private String password;
 
 }
