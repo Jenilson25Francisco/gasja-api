e21274e9807e90b5008bd88c27d332eca367fc4c
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-08 16:28:30 +0100

    feat: add user assembler used for convert dto objects

---
+++ src/main/java/com/zanguetsuinc/gasja_api/api/assembler/userAssembler/UserAssembler.java
@@ -0,0 +1,25 @@
+package com.zanguetsuinc.gasja_api.api.assembler.userAssembler;
+
+import com.zanguetsuinc.gasja_api.api.dto.request.UserRequest;
+import com.zanguetsuinc.gasja_api.api.dto.response.UserResponse;
+import com.zanguetsuinc.gasja_api.domain.models.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public UserAssembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public UserResponse toResponse(User user){
+        return modelMapper.map(user, UserResponse.class);
+    }
+
+    public User toRequest(UserRequest userRequest){
+        return modelMapper.map(userRequest, User.class);
+    }
+}
