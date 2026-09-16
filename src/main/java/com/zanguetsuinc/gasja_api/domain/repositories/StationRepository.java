7f24c41164fda612fd248d2703167ec469f22f38
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-16 18:52:08 +0100

    feat: add method to find station by phone number

--- src/main/java/com/zanguetsuinc/gasja_api/domain/repositories/StationRepository.java
+++ src/main/java/com/zanguetsuinc/gasja_api/domain/repositories/StationRepository.java
@@ -4,8 +4,12 @@ import com.zanguetsuinc.gasja_api.domain.models.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface StationRepository extends JpaRepository<Station, UUID> {
+
+    Optional<Station> findByPhone(String phone);
+
 }
