a1add125f11a1485294f58c09e7bfc3a49791294
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-09-17 21:26:18 +0100

    feat: rename getStationService to getRetailerService and update related references

--- src/main/java/com/zanguetsuinc/gasja_api/domain/services/getRetailerService.java
+++ src/main/java/com/zanguetsuinc/gasja_api/domain/services/getRetailerService.java
@@ -1,22 +1,22 @@
 package com.zanguetsuinc.gasja_api.domain.services;
 
 import com.zanguetsuinc.gasja_api.domain.exceptions.ResourceNotFoundException;
-import com.zanguetsuinc.gasja_api.domain.models.Station;
+import com.zanguetsuinc.gasja_api.domain.models.Retailer;
 import com.zanguetsuinc.gasja_api.domain.repositories.StationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public class getStationService {
+public class getRetailerService {
 
     private final StationRepository stationRepository;
 
-    public getStationService(StationRepository stationRepository) {
+    public getRetailerService(StationRepository stationRepository) {
         this.stationRepository = stationRepository;
     }
 
-    public Station getStation(UUID stationId){
+    public Retailer getStation(UUID stationId){
         return stationRepository.findById(stationId)
                 .orElseThrow(() -> new ResourceNotFoundException("Posto não encontrado"));
     }
